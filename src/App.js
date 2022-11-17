145054300240b69a58cfcca9cbf49b30f1d55820
granpeter <granpeter10@gmail.com>
2022-11-16 22:15:02 -0500

    commit 16/11/2022
    funciones de autenticación y registro de usuarios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import Nav from "./Nav";
 import './App.css';
 import { NavLink} from "react-router-dom";
 import AgregarProducto from './AgregarProducto';
 import ListarProductos from './ListarProductos';
 import ActualizarProducto from './ActualizarProducto';
+import Navbar from "./Navbar";
+import Landing from "./Landing";
+import Register from "./Register";
+import Login    from "./Login";
+import{ Provider} from"react-redux";
+import store from"./store";
+
+
+
+
 
 import {
   Switch,
@@ -17,11 +26,19 @@ function App() {
   
 <div> 
     <Nav></Nav>
+     <Navbar/>
+     <Landing/>
     <div className="section">
       <div className="columns">
-         
+
+      <Provider store={store}>  
           <Switch>
-          
+           
+          <Route exactpath="/"component={Landing}/>
+          <Route exactpath="/register"component={Register}/>
+           <Route exactpath="/login"component={Login}/> 
+
+
            <Route path="/productos/agregar">
                <AgregarProducto></AgregarProducto> 
             </Route>         
@@ -35,7 +52,7 @@ function App() {
             </Route>
           
            </Switch>
-
+        </Provider>    
        </div> 
 
 
